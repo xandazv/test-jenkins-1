5ab8c93f7dbb2f42c1b23fddbbaf444c806f4e78
Alex Zaballa <xandazv@hotmail.com>
2019-10-27 14:37:07 -0400

    Added requirements.txt

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
 
+def sum (a,b):
+    return (a+b)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    res = sum (3,4)
+    return 'Hello, World! %s' %(res)
 
 if __name__ == '__main__':
     app.run()
---
+++ src/test_main.py
@@ -0,0 +1,6 @@
+from main import *
+
+def test_sum():
+    assert sum(3,4) == 7
+
+
